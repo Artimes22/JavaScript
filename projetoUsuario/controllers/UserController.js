ac7a305b43c1f368ee95f92bf60c518e9e777bbb
Artimes22 <artimes.flima@gmail.com>
2023-06-27 19:50:43 -0300

    validando formulario

--- projetoUsuario/controllers/UserController.js
+++ projetoUsuario/controllers/UserController.js
@@ -3,7 +3,7 @@ class UserController {
     constructor(formId, tableId){
 
         this.formEl = document.getElementById(formId);
-        this.tableId = document.getElementById(tableId);
+        this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
         
@@ -20,6 +20,8 @@ class UserController {
             btn.disabled = true;
 
             let values = this.getValues();
+
+            if(!values) return false;
 
             this.getPhoto().then(
                 (content) => {
@@ -90,7 +92,16 @@ class UserController {
 
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if(field.name == "gender"){
     
@@ -111,6 +122,12 @@ class UserController {
             }
     
         });
+
+        if (!isValid){
+
+            return false;
+
+        }
     
         return new User(
             user.name, 
@@ -131,13 +148,15 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'sim' : 'não' }</td>
-            <td>${dataUser.birth}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
@@ -146,7 +165,40 @@ class UserController {
         `;
 
 
-        this.tableId.appendChild(tr) 
+        this.tableEl.appendChild(tr) 
+
+        this.updateCount();
+
+
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 
 }
+
+
+
+
+
+
+
+
+
