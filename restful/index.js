02b2e228331581bf96f324cfbbb439b3159a3968
Artimes22 <artimes.flima@gmail.com>
2023-08-13 17:14:24 -0300

    tentativa de validação

--- restful/index.js
+++ restful/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const consing = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(expressValidator());
 
 consing().include('routes').include('utils').into(app);
 
--- restful/routes/users.js
+++ restful/routes/users.js
@@ -35,6 +35,9 @@ module.exports = app => {
 
     route.post((req, res) => {
 
+        if(!app.utils.validator.user(app, req, res)) return false;
+        
+
         db.insert(req.body, (err, user) => {
 
             if (err) {
@@ -65,6 +68,8 @@ module.exports = app => {
     });    
         
     routeId.put((req, res) =>{
+
+        if(!app.utils.validator.user(app, req, res)) return false;
 
         db.update({_id: req.params.id}, req.body, err=>{
     
