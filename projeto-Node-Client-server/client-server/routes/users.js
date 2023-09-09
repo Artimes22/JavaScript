0450b2c251fc9bc2d06587cfc6564e0b40702c21
Artimes22 <artimes.flima@gmail.com>
2023-09-09 16:03:21 -0300

    Update users.js

--- projeto-Node-Client-server/client-server/routes/users.js
+++ projeto-Node-Client-server/client-server/routes/users.js
@@ -3,19 +3,24 @@ var assert = require('assert');
 var restify = require('restify-clients');
 var router = express.Router();
 
-
 // Creates a JSON client
 var client = restify.createJsonClient({
-  url: 'http://localhost:4000'
+  url: 'http://localhost:4000' // Alterado para a porta correta
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
 
-  client.get('/users', function(err, request, response, obj) {
-    assert.ifError(err);
-  
-    res.end(JSON.stringify(obj, null, 2))
+router.get('/users', function (req, res, next) {
+  client.get('/users', function (err, request, response, obj) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: 'Erro na requisição'
+      });
+    }
+
+    console.log(obj);
+    res.status(200).json(obj);
   });
 });
 
