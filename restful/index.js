007f4d18a9dd08fccaa176f63a317a15e0404f19
Artimes22 <artimes.flima@gmail.com>
2023-10-09 19:42:52 -0300

    a

--- restful/index.js
+++ restful/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const consing = require('consign');
 const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(expressValidator());
+
 
 consing().include('routes').include('utils').into(app);
 
 app.listen(3000, '127.0.0.1', ()=>{
 
+    
     console.log("Servidor esta no ar");
 
 });
